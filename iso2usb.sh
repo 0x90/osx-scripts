2c470629fa0cc01d848f887710501cc18171f431
0x90 <oleg.kupreev@gmail.com>
2015-09-14 16:48:03 +0300

    euid checking

--- iso2usb.sh
+++ iso2usb.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 # Write ISO to USB flash drive
 
-# TODO: check euid
+check_euid(){
+    if [[ $EUID -ne 0 ]]; then
+        print_error "This script must be ran with sudo or root privileges, or this isn't going to work."
+        exit 1
+    fi
+}
+
 # TODO: add checking args
+check_euid
 diskutil unmountDisk $2
 dd if=$1 of=$2 bs=1m && sync
 diskutil eject $2
