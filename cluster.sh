c3f1f6f189661ccabd2fee706a071b98670fddb1
Oleg Kupreev <oleg.kupreev@gmail.com>
2019-03-03 18:46:57 +0300

    Kubernetes cluster

---
+++ cluster.sh
@@ -0,0 +1,17 @@
+brew update
+brew install xhyve
+brew install docker-machine-driver-xhyve
+brew install hyperkit
+brew install docker-machine-driver-hyperkit
+
+# docker-machine-driver-hyperkit need root owner and uid 
+sudo chown root:wheel /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
+sudo chmod u+s /usr/local/opt/docker-machine-driver-hyperkit/bin/docker-machine-driver-hyperkit
+
+minikube config set vm-driver hyperkit
+# minikube start --memory=2048 --vm-driver=xhyve
+#minikube start
+
+
+brew cask install osxfuse
+brew install datawire/blackbird/telepresence
