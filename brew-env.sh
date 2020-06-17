138896fb5db417243d38407e25e38a2c21b33707
0x90 <oleg.kupreev@gmail.com>
2020-06-17 16:22:03 +0300

    added brew-env.sh

--- brew-env.sh
+++ brew-env.sh
@@ -20,3 +20,9 @@ export LDFLAGS="-L/usr/local/opt/readline/lib"
 export CPPFLAGS="-I/usr/local/opt/readline/include"
 export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=(brew --prefix openssl@1.1)"
+
+
+export PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig"
+export CPPFLAGS="-I$(brew --prefix readline)/include"
+export LDFLAGS="-L$(brew --prefix readline)/lib"
+
