d7f1dbef179fff8ad6fc75b39efc49976ad1f47a
0x90 <oleg.kupreev@gmail.com>
2020-06-17 16:20:48 +0300

    added brew-env.sh

--- brew-env.sh
+++ brew-env.sh
@@ -6,3 +6,17 @@ export CPPFLAGS="-I/usr/local/opt/readline/include"
 
 #For pkg-config to find readline you may need to set:
 export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+
+
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/ruby/lib"
+export CPPFLAGS="-I/usr/local/opt/ruby/include"
+export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
+export PATH="/usr/local/opt/openssl@1.1/bin:PATH"
+export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
+export LDFLAGS="-L/usr/local/opt/readline/lib"
+export CPPFLAGS="-I/usr/local/opt/readline/include"
+export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=(brew --prefix openssl@1.1)"
