92a24b9f7fb9f75760a9d8400f4907b594040bbd
0x90 <oleg.kupreev@gmail.com>
2020-08-05 09:36:32 +0300

    devops.sh added

---
+++ devops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+brew install dive
+
+
+# adoptopenjdk/openjdk
+#aquasecurity/trivy
+#brewsci/science
+#cartr/qt4
+#homebrew/cask
+#homebrew/cask-drivers
+#homebrew/cask-fonts
+#homebrew/cask-versions
+#homebrew/core
+#3homebrew/services
+#johanhaleby/kubetail
+#koekeishiya/formulae
+# miderwong/flutter
+# pothosware/pothos
+# ruchirchawdhry/casks
+
+brew tap skanehira/docui
+brew install docui
